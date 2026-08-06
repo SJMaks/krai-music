be828ca1e46f8afb629b4852aa459039c024ab40
SJMaks <kraev.maksim2003@gmail.com>
2026-08-06 14:44:17 +0700

    changed router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -11,7 +11,7 @@ import NotFoundPage from '../pages/NotFoundPage'
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: '/krai-music/',
     element: <Layout />,
     children: [
       { index: true, element: <HomePage /> },
