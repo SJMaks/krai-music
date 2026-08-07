68d832be1773e9165030751517d10f93479078a6
SJMaks <kraev.maksim2003@gmail.com>
2026-08-07 21:28:35 +0700

    fixed cms previews

--- public/admin/preview.js
+++ public/admin/preview.js
@@ -1,6 +1,5 @@
 // ===== 1. Подключаем стили =====
-// Если файл лежит в /admin/preview.css
-CMS.registerPreviewStyle("/public/admin/preview.css");
+CMS.registerPreviewStyle("/preview.css");
 
 // ===== 2. Вспомогательная функция для изображений =====
 function getImageUrl(asset) {
